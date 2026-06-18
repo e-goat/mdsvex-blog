68370cda75f08103ef84bc885f8720b70dbe7fe7
e-goat <gorgonaly@gmail.com>
2026-06-18 22:25:28 +0300

    Include post TAGS

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -17,7 +17,7 @@
             class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl hover:cursor-pointer hover:bg-gray-500/10"
         >
             <div class="w-full max-w-48 overflow-hidden rounded-lg shadow-md">
-                <img class="w-full h-48 object-contain" src={item.thumb} alt={item.slug} />
+                <img class="w-full min-w-48 h-48 object-contain" src={item.thumb} alt={item.slug} />
             </div>
             <div class="flex flex-col gap-2 items-center md:items-start">
                 <span class="text-center md:text-start text-2xl font-medium">{item.title}</span>
@@ -31,6 +31,12 @@
                     <span>{destructoredDate[0]}</span>
                     <!-- year -->
                 </span>
+                <span class="flex flex-wrap gap-2">
+                    {#each item.tags as tag, index (tag)}
+                        <div class="p-1 text-xs rounded-lg shadow-sm text-nowrap text- bg-blue-400/10" data-tag-id={index}>{tag}</div>
+                    {/each}
+                </span>
+                {console.log(item.tags)}
             </div>
         </button>
     {/each}
