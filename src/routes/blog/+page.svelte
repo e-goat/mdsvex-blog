77351b0593627f6ed40524a2c6f91f8c6f25e67f
e-goat <gorgonaly@gmail.com>
2026-06-18 15:28:00 +0300

    /blog page refinements

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -19,15 +19,15 @@
                     onclick={() => goto(resolve(`/blog/${item.slug}`))}
                     class="hover:cursor-pointer hover:bg-gray-500/10"
                 > -->
-                    <div onclick={() => goto(resolve(`/blog/${item.slug}`))} class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl hover:cursor-pointer hover:bg-gray-500/10">
+                    <button onclick={() => goto(resolve(`/blog/${item.slug}`))} class="flex flex-col items-center gap-6 p-7 md:flex-row md:gap-8 rounded-2xl hover:cursor-pointer hover:bg-gray-500/10">
 
                         <div class="w-full max-w-48 overflow-hidden rounded-lg shadow-md">
                             <img class="w-full h-48 object-contain" src={item.thumb} alt={item.slug} />
                         </div>
                         <div class="flex flex-col gap-2 items-center md:items-start">
                             <span class="text-center md:text-start text-2xl font-medium">{item.title}</span>
-                            <span class="text-center md:text-start font-medium text-sky-500">{item.description}</span>
-                            <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+                            <span class="text-center md:text-start">{item.description}</span>
+                            <span class="flex gap-2 font-thin italic text-gray-600 dark:text-gray-400">
                                 <span>{destructoredDate[destructoredDate.length - 1]}</span><!-- day -->
                                 <span>·</span>
                                 <span>{destructoredDate[1]}</span> <!-- month -->
@@ -35,7 +35,7 @@
                                 <span>{destructoredDate[0]}</span> <!-- year -->
                             </span>
                         </div>
-                    </div>
+                    </button>
                 <!-- </button> -->
             <!-- </li> -->
         {/each}
